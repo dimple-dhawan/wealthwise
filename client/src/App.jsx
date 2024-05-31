5ba03dc6b7518dff4c76a0f3034478e3e527d164
Michelle Mah <michellemah2001@gmail.com>
2024-05-31 00:06:23 -0400

    Added code for details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from './components/Header/Header';
 import Expenses from './pages/expenses/expenses.jsx';
+import Details from './pages/details/details.jsx';
+
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +11,7 @@ function App() {
      <Routes>
         {/* <Route path="/" element={<Navigate to="/expenses" />} /> */}
         <Route path="/expenses" element={<Expenses />} />
+        <Route path="/details" element={<Details />} />
       </Routes>
      
     </BrowserRouter>
